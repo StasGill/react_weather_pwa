54a241a14a1e906fd9c23bd5c43db47a7189190d
StasGil <stanislav.gill@jadexconsulting.com>
2024-08-07 16:50:42 +0200

    Add styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import WeatherDisplay from "./components/WeatherDisplay";
 import "./App.css";
+// import { getMockData } from "./helpers";
+import { getWeather, getWeatherByCoords } from "./api/weatherApi";
 
 function App() {
   const { t } = useTranslation();
@@ -10,70 +12,49 @@ function App() {
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
 
-  const fetchWeather = useCallback(
-    async (lat, lon) => {
-      const apiKey = "c87eed3b62ba3e0fb9ab356c0976b931"; // Replace with your API key
-      const url =
-        lat && lon
-          ? `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
-          : `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
-      try {
-        const response = await fetch(url);
-        const data = await response.json();
-        setLoading(false);
-        if (data.cod === 200) {
-          setWeatherData(data);
-          setError("");
-        } else {
-          setError(data.message);
-          setWeatherData(null);
-        }
-      } catch (err) {
-        setLoading(false);
-        setError(t("failed_to_fetch"));
-        setWeatherData(null);
-      }
-    },
-    [city, t]
-  );
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getWeather(city).then(({ data }) => setWeatherData(data));
+  };
 
   useEffect(() => {
     if (navigator.geolocation) {
       setLoading(true);
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          fetchWeather(latitude, longitude);
-        },
-        (error) => {
-          setLoading(false);
-          setError(t("location_error"));
-        }
-      );
+
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+
+        getWeatherByCoords(latitude, longitude)
+          .then(({ data }) => setWeatherData(data))
+          .catch((error) => {
+            setLoading(false);
+            setError(t("location_error"));
+          })
+          .finally(setLoading(false));
+      });
     } else {
       setError(t("geolocation_not_supported"));
     }
-  }, [fetchWeather, t]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>{weatherData ? weatherData?.name : t("welcome")}</h1>
-        <input
-          type="text"
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-          placeholder={t("enter_city")}
-          disabled={loading}
-        />
-        <button onClick={() => fetchWeather()} disabled={loading}>
-          {t("get_weather")}
-        </button>
-        {/* <button onClick={() => fetchWeather()} disabled={loading}>
-          {t("use_location")}
-        </button> */}
+        <form className="Form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            placeholder={t("enter_city")}
+          />
+          <button type="submit">{t("get_weather")}</button>
+        </form>
         {loading && <p>{t("loading")}</p>}
+        <div className="Tile-container">
+          <h1>{weatherData ? weatherData?.name : t("welcome")}</h1>
+          <p>{parseInt(weatherData?.main?.temp)} °C</p>
+        </div>
         {error && (
           <p>
             {t("error")}: {error}
---
+++ src/helpers.js
@@ -0,0 +1,17 @@
+export const getMockData = () => {
+  return {
+    name: "Kowale",
+    cod: 200,
+    main: {
+      feels_like: 22.69,
+      grnd_level: 1006,
+      humidity: 50,
+      pressure: 1014,
+      sea_level: 1014,
+      temp: 23.03,
+      temp_max: 25.64,
+      temp_min: 22.56,
+    },
+    wind: { speed: 20 },
+  };
+};
